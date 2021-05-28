2bbaebef32a8a7694ab974790891be189b2316ae
Charisxxx <1635492505@qq.com>
2021-05-28 14:29:11 +0800

    first commit

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author xxxstart
+ * @create 2021-05-28 14:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
